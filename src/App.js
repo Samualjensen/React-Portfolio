a9420359ae5ed1db6f4d5461e14eb70f1de5bfd3
Sam Jensen <samual.jensen@gmail.com>
2023-03-24 15:01:53 -0700

    added new setup to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, HashRouter } from 'react-router-dom';
+
+import NavBar from './components/NavBar';
+import AboutMe from './components/AboutMe';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Footer from './components/Footer';
+import Resume from './components/Resume';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <HashRouter>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route
+            path="/*"
+            element={<AboutMe />}
+          />
+          <Route
+            path="/about"
+            element={<AboutMe />}
+          />
+          <Route
+            path="/contact"
+            element={<Contact />}
+          />
+          <Route
+            path="/portfolio"
+            element={<Portfolio />}
+          />
+          <Route
+            path="/resume"
+            element={<Resume />}
+          />
+        </Routes>
+        <Footer />
+      </div>
+
+    </HashRouter>
+  )
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
